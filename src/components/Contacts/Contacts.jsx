ebfdf7e31b0291a12d60b0bff6fb8efefc76ff1b
T_Himanshu_Sharma <115513580+Himanshu07-debug@users.noreply.github.com>
2026-01-03 12:28:59 +0530

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Contacts.scss'
 import axios from 'axios';
-import Loader from '../Loader'
-import { getAllContactsApi } from '../../apis/restapis';
+import Loader from '../Loader.jsx'
+import { getAllContactsApi } from '../../apis/restapis.js';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedContact } from '../../store/contactSlice.js'
 
@@ -21,7 +21,10 @@ const Contacts = () => {
 
     const getAllContacts = async () => {
         try {
-            const id = user._id;
+            const currentUser = JSON.parse(localStorage.getItem('user-data'));
+            if (!currentUser || !currentUser._id) return;
+            
+            const id = currentUser._id;
             const url = getAllContactsApi + id;
             const response = await axios.get(url);
             setContactList(response.data);
@@ -34,7 +37,28 @@ const Contacts = () => {
 
     useEffect(() => {
         getAllContacts();
-    }, [contactList]);
+        
+        // Listen for custom event when a contact is added
+        const handleContactAdded = () => {
+            getAllContacts();
+        };
+        
+        window.addEventListener('contactAdded', handleContactAdded);
+        
+        // Also listen for storage changes (when localStorage is updated)
+        const handleStorageChange = (e) => {
+            if (e.key === 'user-data') {
+                getAllContacts();
+            }
+        };
+        
+        window.addEventListener('storage', handleStorageChange);
+        
+        return () => {
+            window.removeEventListener('contactAdded', handleContactAdded);
+            window.removeEventListener('storage', handleStorageChange);
+        };
+    }, []);
 
     // console.log(contactList);
     // console.log(onlineContacts);
