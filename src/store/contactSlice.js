e101e3c288f6abeafd7afce038c1e86d33cea9f9
T_Himanshu_Sharma <115513580+Himanshu07-debug@users.noreply.github.com>
2026-01-04 23:40:15 +0530

    Update contactSlice.js

--- src/store/contactSlice.js
+++ src/store/contactSlice.js
@@ -15,9 +15,13 @@ const contactSlice = createSlice({
         setOnlineContacts : (state, action) => {
             state.onlineContacts = action.payload;
         },
+        clearContactState : (state) => {
+            state.selectedContact = undefined;
+            state.onlineContacts = [];
+        },
     }
 })
 
-export const {setSelectedContact, setOnlineContacts} = contactSlice.actions;
+export const {setSelectedContact, setOnlineContacts, clearContactState} = contactSlice.actions;
 
 export default contactSlice.reducer;
